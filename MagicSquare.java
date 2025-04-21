4b756a03109e2f5af1f2d11d3e4d8038c4e98b64
jrtrj <jerit.reji.dev@gmail.com>
2025-04-21 18:18:27 +0530

    Scope for value:12

--- MagicSquare.java
+++ MagicSquare.java
@@ -1,17 +1,31 @@
-package org.example;
-
 import java.util.ArrayList;
 import java.util.Scanner;
-
 public class MagicSquare{
     private Scanner sc = new Scanner(System.in); 
-    private int[][] square = new int[][];
-    public void readSquare(n) {
+    int n;
+    private MagicSquare(int n) {
+        this.n = n;
+    }
+    private int[][] square = new int[n][n];
+    int value = 0;
+    public void readSquare() {
         for(int i = 0; i < n;i++) {
             for(int j = 0; j < n;j++) {
-                int value = sc.nextInt();
+                value = sc.nextInt();
                 value = square[i][j];            }
         }
         if(value == "\n")
             break;
-}
+    }
+    private int[] sumRow = new int[n];
+    private int[] sumCol = new int[n];
+    private int[] sumDiagnoal = new int[n];
+
+    public boolean isMagicSquare(int[][] square) {
+         for(int i = 0; i < n;i++) {
+            for(int j = 0; j < n;j++) {
+                int sum += square[i][j];
+            }
+         sumRow[i] = sum; 
+         }
+    }
